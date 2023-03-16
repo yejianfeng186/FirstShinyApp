8c9f87b4c472832b628c15021f499919884e3d4f
Jianfeng2 Ye <jianfeng2.ye@sanofi.com>
2023-03-16 17:50:31 +0800

    Signed-off-by: Jianfeng2 Ye <jianfeng2.ye@sanofi.com>

--- app.R
+++ app.R
@@ -13,7 +13,7 @@ library(shiny)
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser DataFrame"),
+    titlePanel("Old Faithful Geyser Data Frame"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
